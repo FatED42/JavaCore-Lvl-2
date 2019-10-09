b53bc85643c6c7c9596a176f2115ff49af793adf
FatED42 <cjkrol42@gmail.com>
2019-10-09 13:54:01 +0300

    Дз к уроку 6

---
+++ Lesson_6/src/Client/Controller.java
@@ -0,0 +1,92 @@
+package Client;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class Controller implements Initializable{
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+    @FXML
+    Button btn1;
+
+    Socket socket;
+    PrintWriter out;
+
+    final String IP_ADRESS = "localhost";
+    final int PORT = 8189;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            socket = new Socket(IP_ADRESS, PORT);
+            Scanner in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
+            Scanner console = new Scanner(System.in);
+
+            Thread t1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        String str = in.nextLine();
+                        if(str.equals("/end")) {
+                            out.println("/end");
+                            break;
+                        }
+                        System.out.println("Server " + str);
+                    }
+                }
+            });
+            t1.start();
+
+            Thread t2 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        System.out.println("Введите сообщение");
+                        String str = console.nextLine();
+                        System.out.println("Сообщение отправлено!");
+                        out.println(str);
+                    }
+                }
+            });
+            t2.setDaemon(true);
+            t2.start();
+
+            try {
+                t1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendMsg() throws IOException {
+        out.println(textField.getText());
+        textField.clear();
+        textField.requestFocus();
+    }
+}
